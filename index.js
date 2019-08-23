1ae9a9a97a6257d4c3eafaeac22c3942d1c78189
Joao <ritterjv@gmail.com>
2019-08-22 18:17:39 -0700

    -

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const commandHandler = require("@sustainer-network/TODO=<some-command>-command-handler");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
+const eventStore = require("@sustainer-network/event-store-js");
+const eventBus = require("@sustainer-network/event-bus");
 
 exports.http = (req, res) => {
-  commandHandler({ body: req.body, tokens: tokensFromReq(req) })
+  commandHandler({
+    body: req.body,
+    tokens: tokensFromReq(req),
+    publishEventFn: event => {
+      eventStore.add(event);
+      eventBus.publish(event);
+    }
+  })
     .then(response => res.send(response))
     .catch(e => res.status(e.statusCode).send(e));
 };
